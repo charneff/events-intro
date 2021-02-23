86f82fc9cabc3a76df406ad4e3915911f08bb92b
Charlotte Neff <charlotte.neff@gmail.com>
2021-02-23 14:51:49 -0500

    soln

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 //startup up routine below --> I want something to happen when the page loads
-
 document.addEventListener('DOMContentLoaded', function() {
-    const firstHeaderOnPage = document.querySelector('h3')
-    firstHeaderOnPage.addEventListener('click', logIt)
+    
+    console.log("The page is loaded!")
     const commentForm = document.getElementById('comment-form')
-    commentForm.addEventListener('submit', (event) => {
-        console.log(event)
+    const commentsContainer = document.getElementById('comments-container')
+    commentForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        console.log(event.target)
+        const userInputField = event.target.querySelector('#new-comment')
+        const userInputString = userInputField.value
+        console.log(userInputString)
+        const PTag = document.createElement('p')
+        PTag.textContent = userInputString
+        commentsContainer.appendChild(PTag)
+        userInputField.value = ""
+    })
+    const helicopterNode = document.getElementById('helicopter-parent')
+    helicopterNode.addEventListener('click', function (event) {
+        console.log(event.target)
+        if (event.target.dataset.name === "alert"){
+            window.alert('HI')
+        } else if (event.target.dataset.name === "log"){
+            window.alert('LOG')
+        } else if (event.target.dataset.name === "error"){
+            window.alert('ERROR')
+        }
     })
 
-})
-function logIt() {
-    console.log("THE h3 WAS CLICKED")
-}
+    const firstHeader = document.querySelector('h3')
+    firstHeader.addEventListener('click', handleOnClick)
+  })
+
+
+
+
+ function handleOnClick (){
+    console.log("BUTTON WAS CLICKED!")} 
 
 //OR
 
